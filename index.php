b62993279f05e1be3a9aba9d1a5f54af5454e3fd
Wei Yang <weix0015@stud.kea.dk>
2025-03-11 11:06:40 +0100

    init repo

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <title>Home</title>
+</head>
+<body>
+  <h1>Home page</h1>
+  <img src="img/test.png" alt="test">
+</body>
+</html>
